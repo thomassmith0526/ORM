059443e56a985918bf442890b093392b3727c094
thomassmith0526 <thomassmith0526@gmail.com>
2024-07-09 18:16:35 -0500

    intger

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
-      include: [{model: Product}],
+      include: [Product],
     });
     res.status(200).json(categoryData);
   } catch (err){
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{model: Product}],
+      include: [Product],
     });
     if (!categoryData) {
       res.status(404).json({message: 'No Category with that id!'})
